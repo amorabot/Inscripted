6520d03ee9a74a75079d55ef21cde2965b38af38
amorabot <danielamorimbessa@hotmail.com>
2024-05-07 13:51:58 -0300

    Sword attack rework & Bow attack bugfix

--- src/main/java/com/amorabot/inscripted/skills/LinalgMath.java
+++ src/main/java/com/amorabot/inscripted/skills/LinalgMath.java
@@ -36,6 +36,7 @@ public class LinalgMath {
         return vec.setX(X).setY(Y);
     }
 
+    //https://math.stackexchange.com/questions/2782529/is-the-maximum-rotation-by-multiplying-a-positive-definite-matrix-is-less-than-9
     //https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
     public static Vector rotateAroundGenericAxis(Vector normalAxis, Vector vec, double angleR){
         return vec.multiply(cos(angleR))
@@ -43,3 +44,23 @@ public class LinalgMath {
                 .add(normalAxis.multiply( (normalAxis.dot(vec)*(1-cos(angleR))) ));
     }
 }
+    /*
+        sin of the angle between 3d vectors
+         | U x V | = | U | * | V | sin(a)
+         sin(a) = |UxV| / |U|*|V|
+
+
+    Parametric 3D circle equation:
+    https://math.stackexchange.com/questions/1184038/what-is-the-equation-of-a-general-circle-in-3-d-space
+    https://www.youtube.com/watch?v=OYgiI5bK4kE
+
+        >> p + r( cos(t) )v1 + r( sin(t) )v2 ; t real
+
+            v1 & v2 -> Orthonormal basis (Orthogonal and normalized vectors)
+
+            p = center
+            r = radius
+            v1 = new base's "y" axis
+            v2 = new base's "x" axis
+            t = angle (rads)
+     */
